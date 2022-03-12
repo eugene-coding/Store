25c8df6bf57dec1377cb2ad3f96e4027403298b2
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-12 12:41:24 +0300

    Added CreatingModels component
    Also removed files of the standard Blazor template and a repeating block with the heading is placed in a separate component

---
+++ Store/Client/Shared/Extensions/CreatingModels.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Client.Shared.Extensions;
+
+public partial class CreatingModels
+{
+    private const string ImagesPath = "images/models";
+
+    [Inject]
+    public IStringLocalizer<CreatingModels> Text { get; init; } = null!;
+}
---
+++ Store/Client/Shared/Templates/BlockHeader.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Client.Shared.Templates;
+
+public partial class BlockHeader
+{
+    [Parameter, EditorRequired]
+    public RenderFragment Title { get; init; } = null!;
+
+    [Parameter]
+    public RenderFragment? Subtitle { get; init; }
+
+    [Parameter]
+    public RenderFragment? Description { get; init; }
+}

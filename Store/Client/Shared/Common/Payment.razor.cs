b9485d3724a8dc7ba3503b594384e9d07c494ee4
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-12 05:25:22 +0300

    Added Payment component

---
+++ Store/Client/Shared/Common/Payment.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Client.Shared.Common;
+
+public partial class Payment
+{
+    private const string ImagesPath = "images/payment";
+
+    [Inject]
+    public IStringLocalizer<Payment> Text { get; init; } = null!;
+}

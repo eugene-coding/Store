d3fc76e3b9aac9828ff3296b43c671cab620468c
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-12 05:15:06 +0300

    Added Mobile Menu Component

---
+++ Store/Client/Shared/Common/Header/MobileMenu.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.JSInterop;
+
+using Store.Client.Resources;
+
+namespace Store.Client.Shared.Common.Header;
+
+public partial class MobileMenu : IAsyncDisposable
+{
+    private IJSObjectReference? _module;
+
+    [Inject]
+    public IStringLocalizer<StoreSettings> StoreSettings { get; init; } = null!;
+
+    [Inject]
+    public IJSRuntime JS { get; init; } = null!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _module = await JS.InvokeAsync<IJSObjectReference>("import", "./Shared/Common/Header/MobileMenu.razor.js");
+        }
+    }
+
+    private async void Close()
+    {
+        if (_module is not null)
+        {
+            await _module.InvokeVoidAsync("close");
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_module is not null)
+        {
+            await _module.DisposeAsync();
+        }
+    }
+}
---
+++ Store/Client/Shared/Common/Header/MobileMenu.razor.js
@@ -0,0 +1,15 @@
+﻿export function open() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.style.left = '0';
+
+    const overlay = document.getElementById('mobile-menu-overlay');
+    overlay.style.display = 'block';
+}
+
+export function close() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.style.left = '-385px';
+
+    const overlay = document.getElementById('mobile-menu-overlay');
+    overlay.style.display = 'none';
+}

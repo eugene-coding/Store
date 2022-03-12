1e35bc3ddddc80cee9aa6d49448bed5bd4a2a2f5
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-12 07:27:13 +0300

    Added Faq List template

---
+++ Store/Client/Shared/Templates/FaqList.razor.cs
@@ -0,0 +1,7 @@
+﻿namespace Store.Client.Shared.Templates;
+
+public partial class FaqList
+{
+    [Parameter]
+    public IReadOnlyCollection<Faq> Items { get; init; } = null!;
+}
---
+++ Store/Shared/Entities/Faq.cs
@@ -0,0 +1,12 @@
+﻿namespace Store.Shared.Entities;
+
+public class Faq
+{
+    public int Id { get; init; }
+    public string Question { get; init; } = null!;
+    public string AnswerBody { get; init; } = null!;
+    public string? AnswerFooter { get; init; }
+
+    public int SortOrder { get; init; }
+    public bool Enabled { get; init; }
+}

d944c271d83084e6b13620b4bfa61cc33d078dfd
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-13 16:28:32 +0300

    Added Aside Menu template

---
+++ Store/Client/Shared/Templates/AsideList.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Client.Shared.Templates;
+
+public partial class AsideList
+{
+    [Parameter]
+    public IEnumerable<Breadcrumb>? Items { get; init; }
+
+    [Inject]
+    public NavigationManager Navigation { get; init; } = null!;
+}

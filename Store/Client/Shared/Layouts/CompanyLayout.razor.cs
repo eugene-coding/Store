17cee21de93d18f103e90be62617e8ac1052d7c2
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-13 17:06:20 +0300

    Added Company layout for company/* pages

---
+++ Store/Client/Shared/Layouts/CompanyLayout.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace Store.Client.Shared.Layouts;
+
+public partial class CompanyLayout
+{
+    private List<Breadcrumb> _asideMenu = null!;
+
+    [Inject]
+    public IStringLocalizer<Company> CompanyText { get; init; } = null!;
+
+    [Inject]
+    public IStringLocalizer<Requisites> RequisitesText { get; init; } = null!;
+
+    [Inject]
+    public IStringLocalizer<Policy> PolicyText { get; init; } = null!;
+
+    protected override void OnInitialized()
+    {
+        _asideMenu = new()
+        {
+            new Breadcrumb(CompanyText["HeadingTitle"], Company.Url),
+            new Breadcrumb(RequisitesText["HeadingTitle"], Requisites.Url),
+            new Breadcrumb(PolicyText["HeadingTitle"], Policy.Url),
+        };
+    }
+}

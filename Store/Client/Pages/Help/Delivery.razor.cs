92e4eb7eb465b007ce3f00481206cddfb56853e2
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-13 13:33:49 +0300

    Added Delivery page

---
+++ Store/Client/Pages/Help/Delivery.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace Store.Client.Pages.Help;
+
+[Route(Url)]
+public partial class Delivery
+{
+    public const string Seo = "delivery";
+    public const string Url = $"{Help.Url}/{Seo}";
+
+    private List<Breadcrumb>? _breadcrumbs;
+
+    [Inject]
+    public IStringLocalizer<Delivery> Text { get; init; } = null!;
+
+    [Inject]
+    public IStringLocalizer<Help> HelpText { get; init; } = null!;
+
+    protected override void OnInitialized()
+    {
+        _breadcrumbs = new()
+        {
+            new Breadcrumb(HelpText["HeadingTitle"], Help.Url),
+            new Breadcrumb(Text["HeadingTitle"], Url)
+        };
+    }
+}

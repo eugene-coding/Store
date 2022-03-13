6980916435c1fa36f00e4661e9a6bde6f80182b1
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-13 16:29:31 +0300

    Added Help Layput for help/* pages

---
+++ Store/Client/Shared/Layouts/HelpLayout.razor.cs
@@ -0,0 +1,35 @@
+﻿using Faq = Store.Client.Pages.Help.Faq;
+
+namespace Store.Client.Shared.Layouts;
+
+public partial class HelpLayout
+{
+    private List<Breadcrumb> _asideMenu = null!;
+
+    [Inject]
+    public IStringLocalizer<Help> HelpText { get; init; } = null!;
+
+    [Inject]
+    public IStringLocalizer<Faq> FaqText { get; init; } = null!;
+
+    [Inject]
+    public IStringLocalizer<Payment> PaymentText { get; init; } = null!;
+
+    [Inject]
+    public IStringLocalizer<Delivery> DeliveryText { get; init; } = null!;
+
+    [Inject]
+    public IStringLocalizer<Warranty> WarrantyText { get; init; } = null!;
+
+    protected override void OnInitialized()
+    {
+        _asideMenu = new()
+        {
+            new Breadcrumb(HelpText["HeadingTitle"], Help.Url),
+            new Breadcrumb(PaymentText["HeadingTitle"], Payment.Url),
+            new Breadcrumb(DeliveryText["HeadingTitle"], Delivery.Url),
+            new Breadcrumb(WarrantyText["HeadingTitle"], Warranty.Url),
+            new Breadcrumb(FaqText["HeadingTitle"], Faq.Url)
+        };
+    }
+}

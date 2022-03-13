c45061687b96f79f80a655c8fa4e43124b8edd6c
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-13 17:05:32 +0300

    Added Schema.org microdata for Faq page

--- Store/Client/Pages/Help/Faq.razor.cs
+++ Store/Client/Pages/Help/Faq.razor.cs
@@ -1,15 +1,18 @@
-﻿using Entity = Store.Shared.Entities.Faq;
+﻿using Microsoft.JSInterop;
+
+using Entity = Store.Shared.Entities.Faq;
 
 namespace Store.Client.Pages.Help;
 
 [Route(Url)]
-public partial class Faq
+public partial class Faq : IAsyncDisposable
 {
     public const string Seo = "faq";
     public const string Url = $"{Help.Url}/{Seo}";
 
     private List<Breadcrumb>? _breadcrumbs;
     private IReadOnlyCollection<Entity>? _questions;
+    private IJSObjectReference? _module;
 
     [Inject]
     public IFaqService Service { get; init; } = null!;
@@ -20,6 +23,9 @@ public partial class Faq
     [Inject]
     public IStringLocalizer<Help> HelpText { get; init; } = null!;
 
+    [Inject]
+    public IJSRuntime JS { get; init; } = null!;
+
     protected override void OnInitialized()
     {
         _breadcrumbs = new()
@@ -29,5 +35,41 @@ public partial class Faq
         };
 
         _questions = Service.Get();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _module = await JS.InvokeAsync<IJSObjectReference>("import", "./Pages/Help/Faq.razor.js");
+        }
+
+        SetSchemaAttributes();
+    }
+
+    private async void SetSchemaAttributes()
+    {
+        if (_module is not null)
+        {
+            await _module.InvokeVoidAsync("setSchemaAttributes");
+        }
+    }
+
+    private async void RemoveSchemaAttributes()
+    {
+        if (_module is not null)
+        {
+            await _module.InvokeVoidAsync("removeSchemaAttributes");
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_module is not null)
+        {
+            RemoveSchemaAttributes();
+
+            await _module.DisposeAsync();
+        }
     }
 }

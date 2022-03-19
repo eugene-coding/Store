36afa2e95c778b286c48e1d06c4a6c0458cbeb2c
Eugene <79318195+eugene-coding@users.noreply.github.com>
2022-03-19 14:20:30 +0300

    Added PersistingComponentStateSubscription for Social component

--- Store/Client/Shared/Common/Social.razor.cs
+++ Store/Client/Shared/Common/Social.razor.cs
@@ -2,15 +2,57 @@
 
 namespace Store.Client.Shared.Common;
 
-public partial class Social
+public partial class Social : IDisposable
 {
-    private IReadOnlyCollection<Models.Social>? _socials;
+    private static IReadOnlyCollection<Models.Social>? s_socials;
+
+    private PersistingComponentStateSubscription _persistingSubscription;
+    private readonly string _persistingKey = "social";
+    private static bool s_persisted = false;
 
     [Inject]
     public ISocialService Service { get; init; } = null!;
 
+    [Inject]
+    public PersistentComponentState ApplicationState { get; init; } = null!;
+
     protected override async Task OnInitializedAsync()
     {
-        _socials = await Service.GetAsync();
+        if (s_socials is null)
+        {
+            RegisterOnPersisting();
+
+            if (!ApplicationState.TryTakeFromJson<IReadOnlyList<Models.Social>>(
+                _persistingKey, out var restored))
+            {
+                s_socials = await Service.GetAsync();
+            }
+            else
+            {
+                s_socials = restored!;
+            }
+        }
+    }
+
+    private void RegisterOnPersisting()
+    {
+        if (!s_persisted)
+        {
+            _persistingSubscription = ApplicationState.RegisterOnPersisting(Persist);
+
+            s_persisted = true;
+        }
+    }
+
+    private Task Persist()
+    {
+        ApplicationState.PersistAsJson(_persistingKey, s_socials);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _persistingSubscription.Dispose();
     }
 }

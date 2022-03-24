332106aa379a75ebd8c4ef7e8ea28ce3a5f9b3f9
Eugene <eugene.coding@gmail.com>
2022-03-24 17:43:47 +0300

    Refactoring the Address component

--- Store/Client/Shared/Common/Address.razor.cs
+++ Store/Client/Shared/Common/Address.razor.cs
@@ -1,10 +1,53 @@
 ﻿namespace Store.Client.Shared.Common;
 
-public partial class Address
+public sealed partial class Address
 {
+    private string _class = "address";
+    private string _fullAddress = string.Empty;
+
     [Inject]
     public IStringLocalizer<StoreSettings> StoreSettings { get; init; } = null!;
 
     [Parameter]
-    public bool WhiteText { get; init; }
+    public bool LightText { get; init; }
+
+    protected override void OnInitialized()
+    {
+        _fullAddress = GetFullAddress();
+    }
+
+    protected override void OnParametersSet()
+    {
+        if (LightText)
+        {
+            _class += " address_light";
+        }
+    }
+
+    private string GetFullAddress()
+    {
+        bool isCityEmpty = string.IsNullOrWhiteSpace(StoreSettings["City"]);
+        bool isStreetEmpty = string.IsNullOrWhiteSpace(StoreSettings["Street"]);
+
+        string address = string.Empty;
+
+        if (isCityEmpty)
+        {
+            if (!isStreetEmpty)
+            {
+                address = StoreSettings["Street"];
+            }
+        }
+        else
+        {
+            address = StoreSettings["City"];
+
+            if (!isStreetEmpty)
+            {
+                address += $", {StoreSettings["Street"]}";
+            }
+        }
+
+        return address;
+    }
 }

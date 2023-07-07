48b14011b972bc80f5e96d3b84116b42c1cbb27b
Evgeny Lyashenko <eugene.code@yandex.ru>
2023-07-07 23:07:46 +0300

    Create Breadcrumb.cs

---
+++ Store/Breadcrumb.cs
@@ -0,0 +1,8 @@
+﻿namespace Store;
+
+/// <summary>
+/// Represents one of the breadcrumb elements.
+/// </summary>
+/// <param name="Name">Page name.</param>
+/// <param name="Url">Page URL.</param>
+public record Breadcrumb(string Name, string? Url = null);
